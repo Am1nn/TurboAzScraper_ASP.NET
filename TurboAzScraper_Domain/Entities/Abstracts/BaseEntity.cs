bb96b7d5098727677e256362e4f53918869520c1
Benna_cw18 <Benna_cw18@STHQ0125-09>
2024-05-24 21:01:33 +0400

    Commit

---
+++ TurboAzScraper_Domain/Entities/Abstracts/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurboAzScraper_Domain.Entities.Abstracts;
+
+public abstract class BaseEntity
+{
+    public int Id { get; set; }
+    public DateTime CreatedAt { get; set; }=DateTime.Now;
+    public DateTime UpdatedAt { get; set; }= DateTime.Now;
+    public bool IsDeleted { get; set; }=false;
+
+}
